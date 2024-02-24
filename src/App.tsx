4649ca8f3b847fde247f59400ed00a96763ffcc8
highbridgejiko <highbridgejiko@gmail.com>
2024-02-24 23:56:48 +0900

    💄 scssを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <div className="App"><h1>discord</h1> </div>
   );
 }
 
